98955f67eb8967a03813089dc056927f045243f3
nguyennhathai <nguyennhathai03@gmail.com>
2016-11-14 10:00:26 +0700

    TestHai

---
+++ TestHai/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestHai.Startup))]
+namespace TestHai
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
